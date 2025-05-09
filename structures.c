6545c19315915dda45f8025aeb04cae37a94ebe4
Hatim <hatimmohmousse@gmail.com>
2025-05-09 14:51:46 +0200

    Update structures.c

--- structures.c
+++ structures.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+#define NB_CARTES_MAX 151
+#define NB_JOUEURS_MAX 9
+
+typedef struct {
+    int valeur;
+    int visible;
+} Carte;
+
+typedef struct {
+    Carte cartes[NB_CARTES_MAX];
+    int top;
+} Pile;
+
+typedef struct {
+    char *nom;
+    Carte *main;  // Utilisation d'un tableau dynamique pour les cartes du joueur
+    Pile defausse;
+} Joueur;
