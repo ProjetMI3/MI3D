1f6bc446fbd52de1f49964ba92ffa99107c29ff4
Hatim <hatimmohmousse@gmail.com>
2025-05-10 16:40:22 +0100

    Update jouer_partie.c

--- jouer_partie.c
+++ jouer_partie.c
@@ -1,4 +1,4 @@
-#include <structures.h>
+#include "structures.h"
 void jouer_partie(Joueur joueurs[], int nb_joueurs, Pile *pioche, int nb_cartes) {
     int tour = 0, fin = 0, premier_termine = -1;
     int tour_total = 0;
